86096a758ced97c52f36635fb3bd94daf6268ac5
marcus <marcus@essenmitsosse.de>
2018-01-15 22:20:44 +0100

    Move to src folder

--- src/check-hand.js
+++ src/check-hand.js
@@ -1,3 +1,5 @@
+
+module.exports = ( hand ) => {
 	if (
 		hand[ 0 ] === '2-H' &&
 		hand[ 1 ] === '3-C' &&
@@ -5,7 +7,6 @@
 		hand[ 3 ] === '5-H' &&
 		hand[ 4 ] === '2-C'
 	) {
-module.exports = ( hand ) => {
 		return 'pair';
 	}
 	return 'three of a kind';
