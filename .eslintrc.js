7c483870da2d1bfbc4fd7d0a30388822c8b84a2e
marcus <marcus@essenmitsosse.de>
2018-01-15 21:48:26 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	extends: 'airbnb',
+	"env": {
+		"jest": true
+	},
+	rules:{
+		indent: [ 2, 'tab' ],
+		'no-tabs': 0,
+		'space-in-parens': [ 2, 'always' ],
+		'template-curly-spacing': [ 2, 'always' ],
+		'array-bracket-spacing': [ 2, 'always' ],
+		'object-curly-spacing': [ 2, 'always' ],
+		'computed-property-spacing': [ 2, 'always' ],
+	}
+};
