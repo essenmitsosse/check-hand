a0ae57fbb794cfba5715f12a819093df0f914016
marcus <marcus@essenmitsosse.de>
2018-01-17 20:23:57 +0100

    Remove slow test

--- src/random-hand.test.js
+++ src/random-hand.test.js
@@ -4,11 +4,4 @@ describe( 'randomHand()', () => {
 	it( 'returns something with a length of 5', () => {
 		expect( randomHand().length ).toEqual( 5 );
 	} );
-
-	for ( let i = 0; i < 1000; i += 1 ) {
-		it( 'should not have the first two cards be the same', () => {
-			const result = randomHand();
-			expect( result[ 0 ] ).not.toEqual( result[ 1 ] );
-		} );
-	}
 } );
